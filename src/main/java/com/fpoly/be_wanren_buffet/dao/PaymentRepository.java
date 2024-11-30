dfb60fb9299624afe15e851e6c9b5366889ab738
NGUYEN THANH LIEM <125434004+liemnt2004@users.noreply.github.com>
2024-11-30 12:12:02 +0700

    Update PaymentRepository.java

--- src/main/java/com/fpoly/be_wanren_buffet/dao/PaymentRepository.java
+++ src/main/java/com/fpoly/be_wanren_buffet/dao/PaymentRepository.java
@@ -3,6 +3,8 @@ package com.fpoly.be_wanren_buffet.dao;
 import com.fpoly.be_wanren_buffet.entity.Order;
 import com.fpoly.be_wanren_buffet.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
@@ -10,4 +12,6 @@ import java.util.Optional;
 @RepositoryRestResource(path = "Payment")
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findByOrder(Order order);
+    @Query("SELECT p FROM Payment p WHERE p.order.orderId = :orderId")
+    Payment findByOrderId(@Param("orderId") Long orderId);
 }
