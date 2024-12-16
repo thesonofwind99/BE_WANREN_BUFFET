67a88fdabe81c8bd42e751d846d679babf1315c6
GiangDev <truonggianga427@gmail.com>
2024-12-16 13:38:30 +0700

    cm

--- src/main/java/com/fpoly/be_wanren_buffet/dao/PromotionRepository.java
+++ src/main/java/com/fpoly/be_wanren_buffet/dao/PromotionRepository.java
@@ -1,16 +1,12 @@
 package com.fpoly.be_wanren_buffet.dao;
 
-import com.fpoly.be_wanren_buffet.dto.PromotionVoucherDTO;
 import org.springframework.data.domain.Page;
 import com.fpoly.be_wanren_buffet.entity.Promotion;
-import com.fpoly.be_wanren_buffet.entity.Voucher;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import java.util.List;
 
 @RepositoryRestResource(path = "Promotion")
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
